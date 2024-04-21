d2b4c385dc18313fba5157d8cce6f8303598c9be
Alex-Nicalace <anicalace@gmail.com>
2024-04-21 09:56:27 +0300

    Search: usage useKey

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useKey } from '../hooks/useKey';
 
 export function Search({
   query,
@@ -7,11 +8,24 @@ export function Search({
   query: string;
   onChangeQuery: (value: string) => void;
 }): JSX.Element {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useKey('Enter', function () {
+    if (document.activeElement === inputRef.current) return;
+    inputRef.current?.focus();
+    onChangeQuery('');
+  });
+
+  useEffect(function () {
+    inputRef.current?.focus();
+  }, []);
+
   return (
     <input
+      ref={inputRef}
       className="search"
       type="text"
-      placeholder="Search movies..."
+      placeholder="Поиск фильмов..."
       value={query}
       onChange={(e) => onChangeQuery(e.target.value)}
     />
